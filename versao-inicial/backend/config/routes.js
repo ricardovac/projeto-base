1468155ee5784ef11e5acce412bbe3e9542ebfd9
typingricky <ricardoviicius@gmail.com>
2022-10-11 23:49:29 -0300

    APIs

---
+++ versao-inicial/backend/config/routes.js
@@ -0,0 +1,29 @@
+module.exports = app => {
+  app.post('/signup', app.api.user.save)
+  app.post('/signin', app.api.auth.signin)
+  app.post('/validateToken', app.api.auth.validateToken)
+
+  app.route('/users')
+    .post(app.api.user.save)
+    .get(app.api.user.get)
+
+  app.route('/users/:id')
+    .put(app.api.user.save)
+    .get(app.api.user.getById)
+
+  app.route('/categories')
+    .get(app.api.category.get)
+    .post(app.api.category.save)
+
+  app.route('/categories/tree')
+    .get(app.api.category.getTree)
+
+  app.route('/categories/:id')
+    .get(app.api.category.getById)
+    .put(app.api.category.save)
+    .delete(app.api.category.remove)
+
+  app.route('/categories/:id/articles')
+    .get(app.api.article.getByCategory)
+
+}
